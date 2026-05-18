c86fa323211446a1862ed52ce2bdd8b0f8accf9f
Pawel Mackiewicz <git@mackiewicz.info>
2026-05-18 10:22:35 +0200

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False;
+
+        chars = {}
+
+        for ch in t:
+            curr_val = chars.get(ch, 0)
+            chars[ch] = curr_val + 1
+        
+#        print(chars)
+
+        for ch in s:
+            if ch in chars:
+                if chars[ch] == 0:
+                    return False
+                chars[ch] -= 1
+            else:
+                return False
+
+#        print(chars)
+        return True;
