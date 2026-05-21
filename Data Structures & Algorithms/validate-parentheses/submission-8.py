c8011326ab5a9e571f94bfbb32a8ee9bf5798cc6
Pawel Mackiewicz <git@mackiewicz.info>
2026-05-21 13:50:19 +0200

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) < 2:
+            return False
+        flag = False
+        brackets = []
+        matches = {'{':'}', '(':')', '[':']'}
+        for bracket in s:
+            if bracket in {'{', '(', '['}:
+                brackets.append(bracket)
+                flag = True
+            elif len(brackets) > 0:
+                last_bracket = brackets.pop()
+                if matches[last_bracket] != bracket:
+                    return False
+            else:
+                return False
+                
+
+        
+        return flag and len(brackets) == 0
