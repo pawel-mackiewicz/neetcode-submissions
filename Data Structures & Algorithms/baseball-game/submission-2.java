ec701c1962850346862c152d3d2f196fb909e03f
Pawel Mackiewicz <git@mackiewicz.info>
2026-05-21 13:32:49 +0200

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Deque<Integer> record = new ArrayDeque<>();
+        for(String op : operations) {
+            if (op.equals("+")) {
+                int a = record.pop();
+                int b = record.pop();
+                record.push(b);
+                record.push(a);
+                record.push(b+a);
+            } else if (op.equals("C")) {
+                record.pop();
+            } else if (op.equals("D")) {
+                record.push(record.peek() * 2);
+            } else {
+                int a = Integer.parseInt(op);
+                record.push(a);
+            }
+        }
+        int sum = 0;
+        for (int a : record) {
+            sum += a;
+        }
+        return sum;
+    }
+}
