485a15f04e7e1826d730d4a867586bc1938d9f5c
Pawel Mackiewicz <git@mackiewicz.info>
2026-05-22 15:18:25 +0200

    Add: implement-stack-using-queues - submission-6

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-6.java
@@ -0,0 +1,35 @@
+class MyStack {
+
+    Queue<Integer> q1 = new LinkedList<>();
+
+    public MyStack() {
+    }
+    
+    public void push(int x) {
+        q1.add(x);
+        for (int i = 0; i < q1.size()-1; i++) {
+            q1.add(q1.poll());
+        }
+        System.out.println("Q1: " + q1.toString());
+    }
+    
+    public int pop() {
+        return q1.poll();
+    }
+    
+    public int top() {
+        return q1.peek();
+    }
+    
+    public boolean empty() {
+        return q1.peek() == null;
+    }
+}
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
