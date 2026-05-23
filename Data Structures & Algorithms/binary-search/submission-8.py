9403fc79b2693f351679298ea697578573d458de
Pawel Mackiewicz <git@mackiewicz.info>
2026-05-23 16:54:43 +0200

    Add: binary-search - submission-8

---
+++ Data Structures & Algorithms/binary-search/submission-8.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            pivot = (high+low) // 2
+            print(f'pivot: {pivot}, high: {high}, low: {low}')
+            if target < nums[pivot]:
+                high = pivot-1
+            elif target > nums[pivot]:
+                low = pivot+1
+            else:
+                return pivot
+        return -1
