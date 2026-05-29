be983b1ea05841db94572609d71d1736fb157ac7
Pawel Mackiewicz <git@mackiewicz.info>
2026-05-29 12:51:20 +0200

    Add: kth-largest-integer-in-a-stream - submission-3

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-3.py
@@ -0,0 +1,22 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = []
+        heapq.heapify(self.heap)
+
+        for i in range(len(nums)):
+            if len(self.heap) < self.k or self.heap[0] < nums[i]:
+                heapq.heappush(self.heap, nums[i])
+            if len(self.heap) > self.k:
+                heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        if len(self.heap) < self.k:
+            heapq.heappush(self.heap, val)
+            return self.heap[0]
+        elif self.heap[0] < val:
+                heapq.heappushpop(self.heap, val)
+        return self.heap[0]
+            
+            
