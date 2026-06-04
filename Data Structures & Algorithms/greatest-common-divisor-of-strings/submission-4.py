2709d126d62256f627f8093c728503c8cb82b4f9
Pawel Mackiewicz <git@mackiewicz.info>
2026-06-04 13:39:14 +0200

    Add: greatest-common-divisor-of-strings - submission-4

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+
+        ln1 = len(str1)
+        ln2 = len(str2)
+
+        divisor = math.gcd(ln1,ln2)
+
+        sub = str1[:divisor]
+        multi1 = ln1 // divisor
+        multi2 = ln2 // divisor
+        sub1 = ""
+        sub2 = ""
+        for i in range(multi1):
+            sub1 += sub
+        for i in range(multi2):
+            sub2 += sub
+        
+        if sub1 == str1 and sub2 == str2:
+            return sub
+    
+        return ""
