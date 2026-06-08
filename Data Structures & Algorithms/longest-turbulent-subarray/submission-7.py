3aa7885c3a451f2ac94fa3900ed3941f801e0462
Pawel Mackiewicz <git@mackiewicz.info>
2026-06-08 13:36:19 +0200

    Add: longest-turbulent-subarray - submission-7

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-7.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        sign = 0
+        L = 0
+        maxLen = 1
+
+        for R in range(1,len(arr)):
+            val = arr[R]
+            prevVal = arr[R-1]
+
+            if sign == -1 and val > prevVal:
+                sign = 1
+            elif sign == 1 and val < prevVal:
+                sign = -1
+            else:
+                if val > prevVal:
+                    sign = 1
+                    L = R - 1
+                elif val < prevVal:
+                    sign = -1
+                    L = R - 1
+                else: 
+                    sign = 0
+
+            if sign != 0:
+                maxLen = max(maxLen, R - L + 1)       
+        
+        return maxLen
