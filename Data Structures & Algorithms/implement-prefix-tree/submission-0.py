65a6420f6cb9bd0c80df63df6be2297540c74757
Pawel Mackiewicz <git@mackiewicz.info>
2026-06-14 08:42:33 +0200

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,38 @@
+class TreeNode:
+
+    def __init__(self):
+        self.chars = {}
+        self.isWord = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.node = TreeNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.node
+        for c in word:
+            if c not in curr.chars:
+                curr.chars[c] = TreeNode()
+            curr = curr.chars[c]
+        curr.isWord = True
+
+
+    def search(self, word: str) -> bool:
+        curr = self.node
+        for c in word:
+            if c not in curr.chars:
+                return False
+            curr = curr.chars[c]
+        return curr.isWord
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.node
+        for c in prefix:
+            if c not in curr.chars:
+                return False
+            curr = curr.chars[c]
+        return True
+        
+        
