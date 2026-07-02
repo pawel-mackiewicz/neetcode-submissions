1d8b0d75520dc4ec89c99c6a6d0f0bfa914ca117
Pawel Mackiewicz <git@mackiewicz.info>
2026-07-02 13:45:33 +0200

    Add: find-the-town-judge - submission-4

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        
+        outgoing = [0] * (n+1) 
+        incoming = [0] * (n+1)
+
+        for t in trust:
+            a, b = t[0], t[1]
+
+            outgoing[a] += 1
+            incoming[b] += 1
+
+        for i in range(1, n+1):
+            if outgoing[i] == 0 and incoming[i] == n-1:
+                    return i
+        
+        return -1
