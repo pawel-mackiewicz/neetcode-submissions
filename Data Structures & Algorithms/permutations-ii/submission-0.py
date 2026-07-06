01be507edacc9958caff59f740aeb608c3962cc7
Pawel Mackiewicz <git@mackiewicz.info>
2026-07-06 13:57:01 +0200

    Add: permutations-ii - submission-0

---
+++ Data Structures & Algorithms/permutations-ii/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        if len(nums) == 0:
+            return [[]]
+
+        res = []
+        perms = self.permuteUnique(nums[1:])
+
+        for p in perms:
+            for i in range(len(p)+1):
+                p_copy = p.copy()
+                p_copy.insert(i,nums[0])
+                if p_copy not in res:
+                    res.append(p_copy)
+        return res
