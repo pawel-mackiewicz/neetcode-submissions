18f4039e92cbe4b6484c33e56b9185d40337e26e
Pawel Mackiewicz <git@mackiewicz.info>
2026-07-10 17:02:06 +0200

    Add: swim-in-rising-water - submission-0

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        t = 0
+        ROWS, COLS = len(grid), len(grid[0])
+
+        reqT = grid[0][0]
+        minHeap = [(reqT, (0,0))]
+        visited = [(0,0)]
+
+        while minHeap:
+            reqT,(i,j) = heapq.heappop(minHeap)
+
+            t = max(t, reqT)
+
+            if i == ROWS-1 and j == COLS-1:
+                return t
+
+            if i - 1 >= 0 and (i-1,j) not in visited:
+                heapq.heappush(minHeap, (grid[i-1][j],(i-1,j)))
+                visited.append((i-1,j))
+            if i + 1 < ROWS and (i+1,j) not in visited:
+                heapq.heappush(minHeap, (grid[i+1][j],(i+1,j)))
+                visited.append((i+1,j))
+            if j - 1 >= 0 and (i, j-1) not in visited:
+                heapq.heappush(minHeap, (grid[i][j-1],(i,j-1)))
+                visited.append((i,j-1))
+            if j + 1 < COLS and (i, j+1) not in visited:
+                heapq.heappush(minHeap, (grid[i][j+1],(i,j+1)))
+                visited.append((i,j+1))
+        
+        return -1
